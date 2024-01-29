20a4cfeb2db468f74cc1cecaf8f3c4165fac8056
Manuel Ceppi <manuel.ceppi@outlook.it>
2024-01-29 22:34:11 +0100

    fixes

--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
 import datetime
 import logging
+import core.trade.trade as trading
+import core.market.market_data as market_data
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -10,11 +12,22 @@ def algo_trade_start_function(event, context):
     # Test purpose print
     # print("Your cron function " + name + " ran at " + str(current_time))
     logger.info("Algo trade bot - Start function")
-    # TODO Choose stocks to trade (env variables?)
-    logger.info("lambda function is running")
+    # TODO Choose stocks to trade (env variables?) Start with AAPL just for testing
+
+    logger.info("Getting stocks to trade")
+    stocks_to_trade = get_stocks_to_trade();
+    logger.info("Trading " + str(stocks_to_trade) + " stocks")
+
+    logger.info("Getting stock data")
+    stock_info = get_stock_data(stocks_to_trade);
 
 def get_stocks_to_trade():
-    return [];
+    #TODO get stocks from env variables
+    return ['AAPL']
+
+def get_stock_data(stocks_to_trade):
+    market_data.get_stocks_data()
+    return []
 
 # To test locally, uncomment the following line
 algo_trade_start_function("event", type('',(object,),{"function_name":"test"})()) 
